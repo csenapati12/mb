abc6f6ba552ff1c22edee9cf30316c170daa877b
Chaitanya Senapati <csenapati12@gmail.com>
2020-11-09 08:42:02 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #baz.sh "$@"
 
-var build_mode =$1
-var release_tag=$2
+MODE1 =$1
+release_tag=$2
 
 echo 'first1 ' $1
 echo '2nd1 ' $2
-echo "222  $build_mode" 
+echo "222  $MODE1" 
 echo "222 $release_tag"
